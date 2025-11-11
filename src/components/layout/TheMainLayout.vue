dfc1083bd096fda91754e83cf84aca0728f53fe2
danielpale <danielpp79@gmail.com>
2025-11-11 17:15:19 -0600

    feat: Se añadió un botón para navegar entre rutas.

--- src/components/layout/TheMainLayout.vue
+++ src/components/layout/TheMainLayout.vue
@@ -1,7 +1,26 @@
-<script setup></script>
+<script setup>
+import { computed } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const routes = computed(() => router.getRoutes().filter((route) => route.path !== '/'))
+</script>
 
 <template>
   <v-app>
+    <div class="pa-4">
+      <v-btn color="red">
+        Routes
+        <v-menu activator="parent">
+          <v-list nav>
+            <v-list-item v-for="route in routes" :key="route.name" :to="{ name: route.name }">
+              {{ route.name }}
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </v-btn>
+    </div>
     <v-main>
       <router-view />
     </v-main>
