437a76020f4c60726661977a2cd327fa96ae3b4f
danielpale <danielpp79@gmail.com>
2025-11-12 14:36:52 -0600

    refactor: Se cambio como se obtienen las posiciones de las cartas del spritesheet.

--- src/components/base/BaseCard.vue
+++ src/components/base/BaseCard.vue
@@ -2,69 +2,12 @@
 import { computed, inject, useTemplateRef, watch } from 'vue'
 import { gsap } from 'gsap'
 
-const POSITIONS = {
-  back: { row: 0, col: 0 },
+import { POSITIONS } from '@/constants/card'
 
-  'yellow-1': { row: 1, col: 0, offsetX: 0, offsetY: 0 },
-  'yellow-2': { row: 1, col: 1, offsetX: 0, offsetY: 0 },
-  'yellow-3': { row: 1, col: 2, offsetX: 3, offsetY: 0 },
-  'yellow-4': { row: 1, col: 3, offsetX: 5, offsetY: 0 },
-  'yellow-5': { row: 1, col: 4, offsetX: 8, offsetY: 0 },
-  'yellow-6': { row: 1, col: 5, offsetX: 10, offsetY: 0 },
-  'yellow-7': { row: 1, col: 6, offsetX: 13, offsetY: 0 },
-  'yellow-8': { row: 1, col: 7, offsetX: 18, offsetY: 0 },
-  'yellow-9': { row: 1, col: 8, offsetX: 20, offsetY: 0 },
-  'yellow-0': { row: 1, col: 9, offsetX: 23, offsetY: 0 },
-  'yellow-plus-two': { row: 1, col: 10, offsetX: 25, offsetY: 0 },
-  'yellow-skip': { row: 1, col: 11, offsetX: 28, offsetY: 0 },
-
-  'yellow-reverse': { row: 2, col: 0, offsetX: 0, offsetY: 3 },
-  'red-1': { row: 2, col: 1, offsetX: 0, offsetY: 3 },
-  'red-2': { row: 2, col: 2, offsetX: 3, offsetY: 3 },
-  'red-3': { row: 2, col: 3, offsetX: 5, offsetY: 3 },
-  'red-4': { row: 2, col: 4, offsetX: 8, offsetY: 3 },
-  'red-5': { row: 2, col: 5, offsetX: 10, offsetY: 3 },
-  'red-6': { row: 2, col: 6, offsetX: 13, offsetY: 3 },
-  'red-7': { row: 2, col: 7, offsetX: 18, offsetY: 3 },
-  'red-8': { row: 2, col: 8, offsetX: 20, offsetY: 3 },
-  'red-9': { row: 2, col: 9, offsetX: 23, offsetY: 3 },
-  'red-0': { row: 2, col: 10, offsetX: 25, offsetY: 3 },
-  'red-plus-two': { row: 2, col: 11, offsetX: 28, offsetY: 3 },
-
-  'red-skip': { row: 3, col: 0, offsetX: 0, offsetY: 6 },
-  'red-reverse': { row: 3, col: 1, offsetX: 0, offsetY: 6 },
-  'blue-1': { row: 3, col: 2, offsetX: 3, offsetY: 6 },
-  'blue-2': { row: 3, col: 3, offsetX: 5, offsetY: 6 },
-  'blue-3': { row: 3, col: 4, offsetX: 8, offsetY: 6 },
-  'blue-4': { row: 3, col: 5, offsetX: 10, offsetY: 6 },
-  'blue-5': { row: 3, col: 6, offsetX: 13, offsetY: 6 },
-  'blue-6': { row: 3, col: 7, offsetX: 18, offsetY: 6 },
-  'blue-7': { row: 3, col: 8, offsetX: 20, offsetY: 6 },
-  'blue-8': { row: 3, col: 9, offsetX: 23, offsetY: 6 },
-  'blue-9': { row: 3, col: 10, offsetX: 25, offsetY: 6 },
-  'blue-0': { row: 3, col: 11, offsetX: 28, offsetY: 6 },
-
-  'blue-plus-two': { row: 4, col: 0, offsetX: 0, offsetY: 9 },
-  'blue-skip': { row: 4, col: 1, offsetX: 0, offsetY: 9 },
-  'blue-reverse': { row: 4, col: 2, offsetX: 3, offsetY: 9 },
-  'green-1': { row: 4, col: 3, offsetX: 5, offsetY: 9 },
-  'green-2': { row: 4, col: 4, offsetX: 8, offsetY: 9 },
-  'green-3': { row: 4, col: 5, offsetX: 10, offsetY: 9 },
-  'green-4': { row: 4, col: 6, offsetX: 13, offsetY: 9 },
-  'green-5': { row: 4, col: 7, offsetX: 18, offsetY: 9 },
-  'green-6': { row: 4, col: 8, offsetX: 20, offsetY: 9 },
-  'green-7': { row: 4, col: 9, offsetX: 23, offsetY: 9 },
-  'green-8': { row: 4, col: 10, offsetX: 25, offsetY: 9 },
-  'green-9': { row: 4, col: 11, offsetX: 28, offsetY: 9 },
-
-  'green-0': { row: 5, col: 0, offsetX: 0, offsetY: 12 },
-  'green-plus-two': { row: 5, col: 1, offsetX: 0, offsetY: 12 },
-  'green-skip': { row: 5, col: 2, offsetX: 3, offsetY: 12 },
-  'green-reverse': { row: 5, col: 3, offsetX: 5, offsetY: 12 },
-}
 const HEIGHT = 256
-const WIDTH = 165
-const MARGIN = 2
+const WIDTH = 164
+const SPRITE_WIDTH = 2048
+const SPRITE_HEIGHT = 2048
 
 const props = defineProps({ card: { type: String, default: 'back' } })
 const model = defineModel({ type: [Boolean, Array, String], default: false })
@@ -74,7 +17,7 @@ const templateRef = useTemplateRef('baseCard')
 const containerTemplateRef = useTemplateRef('baseCardContainer')
 const insideDeck = inject('insideDeck', false)
 
-const position = computed(() => getCardPosition(props.card))
+const style = computed(() => getCardStyle(props.card))
 const selected = computed(() => {
   if (typeof model.value === 'boolean') return model.value
   if (typeof model.value === 'string' || model.value === null) return model.value === props.card
@@ -92,9 +35,14 @@ watch(selected, (value) => {
 })
 
 // Card Functions
-function getCardPosition(card) {
-  const { row, col, offsetX, offsetY } = POSITIONS[card]
-  return `-${col * WIDTH + (col !== 0 ? MARGIN : 0) + offsetX}px -${row * HEIGHT + (row !== 0 ? MARGIN : 0) + offsetY}px`
+function getCardStyle(card) {
+  const style = { backgroundSize: '', backgroundPosition: '' }
+  const { x, y, w, h } = POSITIONS[card] ?? {}
+  const scaleX = WIDTH / w
+  const scaleY = HEIGHT / h
+  style.backgroundSize = `${SPRITE_WIDTH * scaleX}px ${SPRITE_HEIGHT * scaleY}px`
+  style.backgroundPosition = `-${x * scaleX}px -${y * scaleY}px`
+  return style
 }
 function flipCard() {
   const rotationY = !tween?.vars?.rotationY ? 180 : 0
@@ -159,8 +107,8 @@ defineExpose({ flipCard, selectCard, deselectCard, getCard, card: props.card, se
     @click="selected ? deselectCard() : selectCard()"
   >
     <div ref="baseCard" class="base-card">
-      <span class="base-card__front" :style="{ backgroundPosition: position }"></span>
-      <span class="base-card__back"></span>
+      <span class="base-card__front" :style="style"></span>
+      <span class="base-card__back" :style="{ backgroundSize: style.backgroundSize }"></span>
     </div>
   </div>
 </template>
@@ -192,10 +140,6 @@ defineExpose({ flipCard, selectCard, deselectCard, getCard, card: props.card, se
       border-radius: calc(20px + (var(--border-offset) / 2));
       opacity: var(--border-opacity);
     }
-
-    // &:hover::before {
-    //   opacity: 0.4;
-    // }
   }
 
   &__front,
@@ -205,7 +149,7 @@ defineExpose({ flipCard, selectCard, deselectCard, getCard, card: props.card, se
     height: 100%;
     backface-visibility: hidden;
     border-radius: 20px;
-    border: 1px solid black;
+    // border: 1px solid black;
     background-image: url('@/assets/images/uno-spritesheet.png');
   }
 
--- src/views/CardPlaygroundView.vue
+++ src/views/CardPlaygroundView.vue
@@ -3,61 +3,9 @@ import { ref, useTemplateRef } from 'vue'
 
 import BaseCard from '@/components/base/BaseCard.vue'
 
-const cards = [
-  'back',
-  'yellow-1',
-  'yellow-2',
-  'yellow-3',
-  'yellow-4',
-  'yellow-5',
-  'yellow-6',
-  'yellow-7',
-  'yellow-8',
-  'yellow-9',
-  'yellow-0',
-  'yellow-plus-two',
-  'yellow-skip',
-  'yellow-reverse',
-  'red-1',
-  'red-2',
-  'red-3',
-  'red-4',
-  'red-5',
-  'red-6',
-  'red-7',
-  'red-8',
-  'red-9',
-  'red-0',
-  'red-plus-two',
-  'red-skip',
-  'red-reverse',
-  'blue-1',
-  'blue-2',
-  'blue-3',
-  'blue-4',
-  'blue-5',
-  'blue-6',
-  'blue-7',
-  'blue-8',
-  'blue-9',
-  'blue-0',
-  'blue-plus-two',
-  'blue-skip',
-  'blue-reverse',
-  'green-1',
-  'green-2',
-  'green-3',
-  'green-4',
-  'green-5',
-  'green-6',
-  'green-7',
-  'green-8',
-  'green-9',
-  'green-0',
-  'green-plus-two',
-  'green-skip',
-  'green-reverse',
-]
+import { POSITIONS } from '@/constants/card'
+
+const cards = Object.keys(POSITIONS)
 
 const cardsRef = useTemplateRef('cardsRef')
 const cardsSelected = ref([])
@@ -78,7 +26,7 @@ function flipSelectedCards() {
 }
 
 const baseCardRef = useTemplateRef('baseCard')
-const card = ref('red-8')
+const card = ref('yellow-1')
 function flipCard() {
   const flipped = baseCardRef.value.flipCard()
   if (flipped) return
