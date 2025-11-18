369e0c4c4cbb534c2f93d787243088f4e0273958
danielpale <danielpp79@gmail.com>
2025-11-18 16:34:30 -0600

    feat: Creación del componente BaseSection

---
+++ src/components/base/BaseSection.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="base-section">
+    <slot></slot>
+  </section>
+</template>
+
+<script></script>
+
+<style scoped>
+.base-section {
+  max-width: 1920px;
+  margin-inline: auto;
+  height: 100%;
+}
+</style>
