22abdca7b98cb07edbf425164fa59c36b86a86d1
danielpale <danielpp79@gmail.com>
2025-11-25 16:29:41 -0600

    feat: Se añadió GameViewPlayground a CardPlayground

--- src/views/CardPlaygroundView.vue
+++ src/views/CardPlaygroundView.vue
@@ -2,8 +2,12 @@
 import { ref, useTemplateRef } from 'vue'
 
 import BaseCard from '@/components/base/BaseCard.vue'
+import BaseSection from '@/components/base/BaseSection.vue'
+import PlayerItem from '@/components/common/PlayerItem.vue'
+import BaseDeck from '@/components/base/BaseDeck.vue'
 
 import { POSITIONS } from '@/constants/card'
+import { PLAYERS_COORDS } from '@/constants/players'
 
 const cards = Object.keys(POSITIONS)
 
@@ -33,11 +37,28 @@ function flipCard() {
   const index = Math.floor(Math.random() * cards.length)
   card.value = cards[index]
 }
+
+// Game View Playground
+const players = ['danielpale', 'vninomtz', 'aaronhdz', 'mahrafrnc', 'spiderman', 'leomessi']
+const numPlayers = ref(6)
+
+const gvCards = [
+  'red-1',
+  'red-2',
+  'red-3',
+  'red-4',
+  'red-5',
+  'red-6',
+  'red-7',
+  'red-8',
+  'red-9',
+  'red-0',
+]
 </script>
 
 <template>
   <section class="card-playground">
-    <div>
+    <div style="height: 100dvh">
       <div class="grid">
         <base-card ref="baseCard" :card="card" />
       </div>
@@ -61,6 +82,20 @@ function flipCard() {
         <v-btn @click="flipSelectedCards">Flip selected</v-btn>
       </div>
     </div>
+    <base-section class="game-view">
+      <player-item
+        v-for="player in numPlayers"
+        :key="players[player - 1]"
+        :username="players[player - 1]"
+        :col="PLAYERS_COORDS[numPlayers][player - 1].col"
+        :row="PLAYERS_COORDS[numPlayers][player - 1].row"
+      />
+      <base-deck :cards="gvCards" :card-height="160" :card-width="102" />
+      <div class="btns">
+        <v-btn :disabled="numPlayers <= 1" @click="numPlayers--">Rest player</v-btn>
+        <v-btn :disabled="numPlayers >= 6" @click="numPlayers++">Add player</v-btn>
+      </div>
+    </base-section>
   </section>
 </template>
 
@@ -70,10 +105,6 @@ function flipCard() {
   display: flex;
   flex-direction: column;
   gap: 16px;
-
-  & > div {
-    min-height: 100vh;
-  }
 }
 
 .grid {
@@ -81,5 +112,24 @@ function flipCard() {
   display: grid;
   place-items: center;
   height: 80%;
+}
+
+// Game View
+.game-view {
+  display: grid;
+  grid-template-columns: repeat(12, minmax(0, 1fr));
+  grid-template-rows: repeat(12, minmax(0, 1fr));
+
+  & > .base-deck {
+    grid-column: 1 / -1;
+    grid-row: 10 / -1;
+  }
+}
+
+.btns {
+  grid-column: 6 / 8;
+  grid-row: 6 / 8;
+  display: flex;
+  gap: 8px;
 }
 </style>
